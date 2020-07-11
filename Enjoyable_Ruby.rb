703f54ef5f7630451ede13d213156b1cfa95c6b1
hyuna-hub <61468181+hyuna-hub@users.noreply.github.com>
2020-07-11 19:09:41 +0900

    Add files via upload

--- Enjoyable_Ruby.rb
+++ Enjoyable_Ruby.rb
@@ -1,20 +1,13 @@
 print ("Hello, Ruby.\n")
-
 print ("Hello, RUBY!\n")
-
 print ("Hello, \nRuby\n!\n")
-
 print ("Hello \\ Ruby!")
-
 print ('Hello, \nRuby\n!\n')
-
 print ('Hello, \\ \'Ruby\'.')
-
 print "Hello, ", "Ruby", ".", "\n"
 
 
 puts "Hello, Ruby."
-
 puts "Hello,", "Ruby."
 
 
@@ -27,5 +20,10 @@ puts "Hello,\n\tRuby."
 p "Hello,\n\tRuby."
 
 
+print "いずれの御時にか女御更衣あまたさぶらいたまいけるなかに\n"
+print "いとやむごとなき際にはあらぬがすぐれて時めきたまふありけり\n"
+
+
+# encoding: UTF-8
 print "いずれの御時にか女御更衣あまたさぶらいたまいけるなかに\n"
 print "いとやむごとなき際にはあらぬがすぐれて時めきたまふありけり\n"
